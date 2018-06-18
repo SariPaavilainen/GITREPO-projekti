84cff7a37f2f2dc57fc92c5dc440c619bf52e284
SariPaavilainen <sari.paavilainen@iki.fi>
2018-06-18 13:30:22 +0300

    Pohjaprojekti

---
+++ DigiTrafficTester/Program.cs
@@ -0,0 +1,80 @@
+﻿using RataDigiTraffic.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigiTrafficTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ei käytetä komentoriviparametrejä vaan kysytään käyttäjältä, siten voidaan myös loopata tarvittaessa uusiin kysymyksiin
+            if (args.Length == 0)
+            {
+                printUsage();
+                return;
+            }
+            if (args[0].ToLower().StartsWith("-a"))
+            {
+                string asema = "";
+                if (args.Length > 1)
+                {
+                    asema = args[1];
+                }
+                tulostaAsemat(asema);
+                return;    
+            }
+            if (args[0].ToLower().StartsWith("-j"))
+            {
+                string lähtöasema;
+                string kohdeasema;
+                if (args.Length < 3)
+                {
+                    printUsage();
+                    return;
+                }
+                lähtöasema = args[1];
+                kohdeasema = args[2];
+                tulostaJunatVälillä(lähtöasema, kohdeasema);
+            }
+        }
+
+        private static void tulostaJunatVälillä(string lähtöasema, string kohdeasema)
+        {
+            RataDigiTraffic.APIUtil rata = new RataDigiTraffic.APIUtil();
+
+            List<Juna> junat = rata.JunatVälillä(lähtöasema, kohdeasema);
+            string s =  string.Join(", ", junat.Select(j => j.trainNumber + " " + j.trainType));
+            Console.WriteLine($"Junat {lähtöasema} ==> {kohdeasema}: " + s);
+        }
+
+        private static void tulostaAsemat(string asema)
+        {
+            List<Liikennepaikka> paikat;
+            RataDigiTraffic.APIUtil rata = new RataDigiTraffic.APIUtil();
+            paikat = rata.Liikennepaikat();
+            foreach (var item in paikat.Where(p => p.type == "STATION"))
+            {
+                if (item.stationName.StartsWith(asema))
+                {
+                    Console.WriteLine(item.stationName + " - " + item.stationShortCode);
+                }
+            }
+        }
+
+        private static void printUsage()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Ohje:");
+            Console.WriteLine("DigiTrafficTester -a[semat] <asemanAlkukirjain>");
+            Console.WriteLine("tai");
+            Console.WriteLine("DigiTrafficTester -j[unat] alkuasemaLyhenne loppuasemaLyhenne");
+            Console.WriteLine();
+        }
+
+
+    }
+}
---
+++ RataDigiTraffic/APIUtil.cs
@@ -0,0 +1,76 @@
+﻿using Newtonsoft.Json;
+using RataDigiTraffic.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RataDigiTraffic
+{
+    public class APIUtil
+
+    {
+public List<Liikennepaikka> Liikennepaikat()
+        {
+            //Mitä tässä tapahtuu?? :D Lista muodostuu siitä, että metadata/stations -data luetaan response-muuttujaan ja siitä edelleen
+            //responseStringiin ja jsoniin; 
+            string json = "";
+            using (var client = new HttpClient())
+            {           
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                var response = client.GetAsync($"https://rata.digitraffic.fi/api/v1/metadata/stations").Result;
+                var responseString = response.Content.ReadAsStringAsync().Result;
+                json = responseString;
+            }
+            List<Liikennepaikka> res;
+            res = JsonConvert.DeserializeObject<List<Liikennepaikka>>(json);
+            return res;
+            // ja kaivaa jsonista Liikennepaikka-datan list-liikennepaikka-tyyppiseksi listaksi
+        }
+
+        public List<Juna> JunatVälillä(string mistä, string minne)
+        {
+            //Sama juttu paitsi että Juna-tyyppinen lista kahden aseman välillä 
+            string json = "";
+            string url = $"https://rata.digitraffic.fi/api/v1/schedules?departure_station={mistä}&arrival_station={minne}";
+
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                var response = client.GetAsync(url).Result;
+                var responseString = response.Content.ReadAsStringAsync().Result;
+                json = responseString;
+            }
+            List<Juna> res;
+            res = JsonConvert.DeserializeObject<List<Juna>>(json);
+            return res;
+        }
+
+        public List<Kulkutietoviesti> LiikennepaikanJunat(string paikka )
+        {
+            // Sama juttu kuin yllä, mutta tietyn parametrina annettavan paikan kautta kulkevat junat tänään
+            string json = "";
+            string url = $"https://rata.digitraffic.fi/api/v1/train-tracking?station={paikka}&departure_date={DateTime.Today.ToString("yyyy-MM-dd")}";
+
+            using (var client = new HttpClient())
+            {
+
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                var response = client.GetAsync(url).Result;
+                var responseString = response.Content.ReadAsStringAsync().Result;
+                json = responseString;
+            }
+            List<Kulkutietoviesti> res;
+            res = JsonConvert.DeserializeObject<List<Kulkutietoviesti>>(json);
+            return res;
+        }
+    }
+
+
+
+
+
+}
