e33dc40a56714571e2521deaf9bc52ea95fa56b1
hmlapp <hannamari.lapp@gmail.com>
2018-06-19 09:36:56 +0300

    a

--- RataDigiTraffic/APIUtil.cs
+++ RataDigiTraffic/APIUtil.cs
@@ -13,13 +13,13 @@ namespace RataDigiTraffic
     public class APIUtil
 
     {
-public List<Liikennepaikka> Liikennepaikat()
+        public List<Liikennepaikka> Liikennepaikat()
         {
-            //Mitä tässä tapahtuu?? :D Lista muodostuu siitä, että metadata/stations -data luetaan response-muuttujaan ja siitä edelleen
-            //responseStringiin ja jsoniin; 
+            //            //Mitä tässä tapahtuu?? :D Lista muodostuu siitä, että metadata/stations -data luetaan response-muuttujaan ja siitä edelleen
+            //            //responseStringiin ja jsoniin; 
             string json = "";
             using (var client = new HttpClient())
-            {           
+            {
                 client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
                 var response = client.GetAsync($"https://rata.digitraffic.fi/api/v1/metadata/stations").Result;
                 var responseString = response.Content.ReadAsStringAsync().Result;
@@ -33,7 +33,7 @@ public List<Liikennepaikka> Liikennepaikat()
 
         public List<Juna> JunatVälillä(string mistä, string minne)
         {
-            //Sama juttu paitsi että Juna-tyyppinen lista kahden aseman välillä 
+//            //Sama juttu paitsi että Juna-tyyppinen lista kahden aseman välillä 
             string json = "";
             string url = $"https://rata.digitraffic.fi/api/v1/schedules?departure_station={mistä}&arrival_station={minne}";
 
